f7d2c17d516850d3cfdc2d9c08f265ffb182ce3b
chigeo <misasagi.119@gmail.com>
2016-09-19 12:28:37 +0900

    ScriptTest　C#の基本
    C#を使用してのプログラミング入門

---
+++ Assets/Test.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		int[] array = {13,32,8,16,34} ;
+		for(int c = 0; c < array.Length; c++){
+			Debug.Log(array[c]);
+		}
+
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
